f16315f5904feb898500beebe116412bab0156a4
sirprize <hoegl@backdoor.ch>
2023-03-05 15:14:34 +0100

    Fix deprecation

--- lib/Batch/Task/Task.php
+++ lib/Batch/Task/Task.php
@@ -75,7 +75,7 @@ class Task
         }
         catch (\Exception $e)
         {
-            throw new RuntimeException(sprintf('Batch task error'), null, $e);
+            throw new RuntimeException(sprintf('Batch task error'), 0, $e);
         }
     }
 
